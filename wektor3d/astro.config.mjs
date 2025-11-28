2a33e9d462493217340fdb42ab2ca8534b738919
h4q <k0k0sek@gmail.com>
2025-11-28 14:55:25 +0100

    Add sitemap integration

--- wektor3d/astro.config.mjs
+++ wektor3d/astro.config.mjs
@@ -2,14 +2,20 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.wektor3d.pl',
+
   image: {
     domains: ["images.unsplash.com"],
     service: { entrypoint: 'astro/assets/services/sharp' }
   },
+
   vite: {
     plugins: [/** @type {any} */ (tailwindcss())],
   },
+
+  integrations: [sitemap()],
 });
